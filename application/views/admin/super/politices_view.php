a715a17e5623586febaeee2fb9536d70e68cc969
Andrés David Montoya Aguirre <admont28@gmail.com>
2016-04-27 17:08:39 -0500

    Adición de políticas de GESEN-UQ
    * Se adiciona las políticas de uso de GESEN-UQ tanto en la ventana modal
    como en la página de políticas de gesen.

--- application/views/admin/super/politices_view.php
+++ application/views/admin/super/politices_view.php
@@ -7,7 +7,37 @@
     <h3>Políticas de GESEN-UQ</h3>
     <div class="row">
         <div class="col-lg-10 col-lg-offset-1">
-        	<p>Políticas de GESEN-UQ...</p>
+        	<h4>GESEN-UQ podrá ser utilizada teniendo en consideración las siguientes políticas:</h4>
+            <h5>Usuarios y contraseñas:</h5>
+            <p>
+                <ul>
+                    <li class="text-justify">A cada dependencia que lo requiera se le activará solo un usuario y contraseña.</li>
+                    <li class="text-justify">La solicitud de esta activación deberá realizarla solamente el jefe/director/coordinador de la dependencia.</li>
+                    <li class="text-justify">El estándar del nombre de usuario será acorde con el nombre de la dependencia.</li>
+                    <li class="text-justify">La plataforma permite asignar una clave inicial, el usuario es responsable de cambiar la clave al ingresar la primera vez y debe realizar este proceso periódicamente.</li>
+                </ul>
+            </p>
+            <h5>Control de aprobación de la encuesta:</h5>
+            <p>
+                <ul>
+                    <li class="text-justify">El responsable de la gobernabilidad de la plataforma validará que no hayan encuesta solicitadas para un mismo público objetivo y en fechas iguales.</li>
+                    <li class="text-justify">Si los correos pertenecientes al público objetivo no corresponden al dominio uniquindio.edu.co y uqvirtual.edu.co y además es mayor a 1.000 personas se deberá solicitar soporte y validación al Centro de Sistemas y Nuevas Tecnologías.</li>
+                    <li class="text-justify">Los objetivos y contenidos de la encuesta deben ser acordes con las necesidades de la institución acatando normas, reglamentos, políticas y valores institucionales.</li>
+                </ul>
+            </p>
+            <h5>Tiempo máximo de almacenamiento:</h5>
+            <p>
+                <ul>
+                    <li class="text-justify">Los resultados obtenidos estarán disponibles en línea máximo 12 meses a partir de la fecha de cierre de la encuesta.</li>
+                </ul>
+            </p>
+            <h5>Confidencialidad de la información:</h5>
+            <p>
+                <ul>
+                    <li class="text-justify">La persona que gobierna la encuesta no deberá manipular o alterar datos de resultados obtenidos en las encuestas. Así como tampoco deberá difundir información de resultados a otras dependencias diferentes al dueño de la encuesta.</li>
+                    <li class="text-justify">Siempre se deberá velar por la confidencialidad de la información exceptuando requerimientos de auditoria especiales.</li>
+                </ul>
+            </p>
         </div>
     </div>
 </div>
